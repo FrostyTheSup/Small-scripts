71b8961b7f47e741633a25f69fdef9998c91336c
Connor Geiger <connor@lenny.fritz.box>
2020-07-06 17:54:28 +1000

    maybe I'm a genius, maybe I broke it

--- play.sh
+++ play.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
+PLAY=''
 if [ "$1" == "next" ]
 then
 	LASTEPISODE=$(ls -ltrh| tail -n 1| awk '{print $9}')
 	SEASON=${LASTEPISODE: 1:2}
 	EPISODE=${LASTEPIDODE: -2}
-	NEXTEPISODE=$(printf %02d "$((10#$EPISODE + 1))")
+	NEXTEPISODE=${LASTEPISODE: 0: 4}$(printf %02d "$((10#$EPISODE + 1))")
+	if [ -f "$NEXTEPISODE" ]
+	then
+		PLAY=$NEXTEPISODE
+	else
+		PLAY="S$(printf %02d "$((10#$SEASON + 1))")E01"
+	fi
+else
+	PLAY="$1"
+fi
 
-touch "$1"
-vlc "$1" -f >/dev/null 2>&1&
+touch "$PLAY"
+vlc "$PLAY*" -f >/dev/null 2>&1&
 exit 0
-
-# New feature (play.sh next): plays video after the most recently played
