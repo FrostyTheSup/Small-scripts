1c37954dd77de2409b514972176951186307810b
FrostyTheSup <congeiger@gmail.com>
2020-09-01 01:14:48 +1000

    fixed 'play.sh next' in cases where season and episode directly followed by file extension

--- play.sh
+++ play.sh
@@ -5,7 +5,7 @@ if [ "$1" == "next" ]
 then
 	LASTEPISODE=$(ls -ltrh| tail -n 1| awk '{print $9}')
 	SEASON=${LASTEPISODE: 1:2}
-	EPISODE=${LASTEPISODE: -2}
+	EPISODE=${LASTEPISODE: 4:2}
 	NEXTEPISODE=${LASTEPISODE: 0: 4}$(printf %02d "$((10#$EPISODE + 1))")
 	
 	if ls ${NEXTEPISODE}* 1> /dev/null 2>&1
